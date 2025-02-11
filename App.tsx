2a3003a8246ec557108761b2a829d6e939f21573
Andriano041 <s22210216@student.unklab.ac.id>
2025-02-11 14:23:41 +0800

    pertemuan2

--- App.tsx
+++ App.tsx
@@ -1,10 +1,44 @@
-import {Text} from 'react-native' 
+import {Text, View, StyleSheet} from 'react-native' 
 
 const App = () => {
   return (
     //JSX
-    <Text> hello World thanks god</Text>
+    <View style={styles.wrapper}>
+    <View style={styles.container}>
+      <Text style={styles.title}>basic react native</Text> 
+    </View>
+    <View style={styles.container}>
+      <Text style={styles.title}>learn to basic</Text> 
+    </View>
+
+    </View>
+   
+    
+    
+    
+   
   )
 }
 
 export default App
+
+//4. Styling
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'red',
+    borderColor: 'black',
+    borderWidth: 5,
+    padding: 20,
+    margin: 20,
+    borderRadius: 20
+  },
+  title: {
+    fontSize: 50, 
+    fontWeight: '800',
+    color: 'white',
+    textAlign: 'center'
+    
+
+    
+  }
+})
